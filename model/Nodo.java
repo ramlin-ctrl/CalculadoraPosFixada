c543dfec435b4fadb9b2c2ee465f801be7876e12
ramlin-ctrl <nilmarsilva852@gmail.com>
2025-06-10 00:24:55 -0300

    Update Nodo.java

--- model/Nodo.java
+++ model/Nodo.java
@@ -1,9 +1,19 @@
 package model;
 
+/**
+ * Nodo genérico usado para armazenar dados em estruturas encadeadas.
+ *
+ * @param <T> Tipo do dado armazenado no nodo.
+ */
 public class Nodo<T> {
     T dado;
     Nodo<T> proximo;
 
+    /**
+     * Construtor do nodo, inicializa com o dado e próximo nodo nulo.
+     *
+     * @param dado O dado a ser armazenado no nodo.
+     */
     public Nodo(T dado) {
         this.dado = dado;
         this.proximo = null;
